a24e0352d3c8b7d39afab21ad557117299a30f10
joe.yu <joe.yu@mail.haoyisheng.com>
2017-11-24 13:47:55 +0800

    Comment out TryResolvingHost(), because it does not work for some environment...

--- src/NAppUpdate.Framework/Sources/SimpleWebSource.cs
+++ src/NAppUpdate.Framework/Sources/SimpleWebSource.cs
@@ -6,89 +6,87 @@ using System.IO;
 
 namespace NAppUpdate.Framework.Sources
 {
-	public class SimpleWebSource : IUpdateSource
-	{
-		public IWebProxy Proxy { get; set; }
-		public string FeedUrl { get; set; }
+    public class SimpleWebSource : IUpdateSource
+    {
+        public IWebProxy Proxy { get; set; }
+        public string FeedUrl { get; set; }
 
-		public SimpleWebSource()
-		{
-			Proxy = null;
-		}
+        internal SimpleWebSource()
+        {
+            Proxy = null;
+        }
 
-		public SimpleWebSource(string feedUrl)
-		{
-			FeedUrl = feedUrl;
-			Proxy = null;
-		}
+        public SimpleWebSource(string feedUrl)
+        {
+            if (string.IsNullOrEmpty(feedUrl))
+            {
+                throw new ArgumentException("feedUrl is empty");
+            }
 
-		#region IUpdateSource Members
+            FeedUrl = feedUrl;
+            Proxy = null;
+        }
 
-		/// <summary>
-		/// Speed up web request by trying to use dns to resolve ip address.  If it fails then fail early.
-		/// </summary>
-		/// <returns></returns>
-		private void TryResolvingHost()
-		{
-			var uri = new Uri(FeedUrl);
-			try
-			{
-				Dns.GetHostEntry(uri.Host);
-			}
-			catch (Exception e)
-			{
+        #region IUpdateSource Members
+
+        /// <summary>
+        /// Speed up web request by trying to use dns to resolve ip address.  If it fails then fail early.
+        /// </summary>
+        /// <returns></returns>
+        private void TryResolvingHost()
+        {
+            var uri = new Uri(FeedUrl);
+            try
+            {
+                Dns.GetHostEntry(uri.Host);
+            }
+            catch (Exception e)
+            {
                 // eat this exception since sometimes the network works but this method failed...
-				UpdateManager.Instance.Logger.Log(string.Format("Failed to resolve {0}. Check your connectivity. {1}{2}", uri.Host),
+                UpdateManager.Instance.Logger.Log(string.Format("Failed to resolve {0}. Check your connectivity. {1}{2}", uri.Host),
                     WebExceptionStatus.ConnectFailure, Environment.NewLine, e);
-			}
-		}
+            }
+        }
 
-		public string GetUpdatesFeed()
-		{
-			TryResolvingHost();
+        public string GetUpdatesFeed()
+        {
+            // Comment out TryResolvingHost(), because it does not work for some environment...
+            //TryResolvingHost();
 
-			var data = string.Empty;
-			var request = WebRequest.Create(FeedUrl);
-			request.Method = "GET";
-			request.Proxy = Proxy;
-			using (var response = request.GetResponse())
-			{
-				var stream = response.GetResponseStream();
+            var data = string.Empty;
 
-				if (stream != null)
-					using (var reader = new StreamReader(stream, true))
-					{
-						data = reader.ReadToEnd();
-					}
-			}
+            using (var wc = new WebClient())
+            {
+                data = wc.DownloadString(FeedUrl);
+            }
 
-			return data;
-		}
+            return data;
+        }
 
-		public bool GetData(string url, string baseUrl, Action<UpdateProgressInfo> onProgress, ref string tempLocation)
-		{
-			FileDownloader fd;
-			// A baseUrl of http://testserver/somefolder with a file linklibrary.dll was resulting in a webrequest to http://testserver/linklibrary
-			// The trailing slash is required for the Uri parser to resolve correctly.
-			if (!string.IsNullOrEmpty(baseUrl) && !baseUrl.EndsWith("/")) baseUrl += "/";
-			if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
-				fd = new FileDownloader(url);
-			else if (Uri.IsWellFormedUriString(baseUrl, UriKind.Absolute))
-				fd = new FileDownloader(new Uri(new Uri(baseUrl, UriKind.Absolute), url));
-			else
-				fd = string.IsNullOrEmpty(baseUrl) ? new FileDownloader(url) : new FileDownloader(new Uri(new Uri(baseUrl), url));
+        public bool GetData(string url, string baseUrl, Action<UpdateProgressInfo> onProgress, ref string tempLocation)
+        {
+            FileDownloader fd;
+            // A baseUrl of http://testserver/somefolder with a file linklibrary.dll was resulting in a webrequest to http://testserver/linklibrary
+            // The trailing slash is required for the Uri parser to resolve correctly.
+            if (!string.IsNullOrEmpty(baseUrl) && !baseUrl.EndsWith("/")) baseUrl += "/";
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                fd = new FileDownloader(url);
+            else if (Uri.IsWellFormedUriString(baseUrl, UriKind.Absolute))
+                fd = new FileDownloader(new Uri(new Uri(baseUrl, UriKind.Absolute), url));
+            else
+                fd = string.IsNullOrEmpty(baseUrl) ? new FileDownloader(url) : new FileDownloader(new Uri(new Uri(baseUrl), url));
 
-			fd.Proxy = Proxy;
+            fd.Proxy = Proxy;
 
-			if (string.IsNullOrEmpty(tempLocation) || !Directory.Exists(Path.GetDirectoryName(tempLocation)))
-				// WATCHOUT!!! Files downloaded to a path specified by GetTempFileName may be deleted on
-				// application restart, and as such cannot be relied on for cold updates, only for hot-swaps or
-				// files requiring pre-processing
-				tempLocation = Path.GetTempFileName();
+            if (string.IsNullOrEmpty(tempLocation) || !Directory.Exists(Path.GetDirectoryName(tempLocation)))
+                // WATCHOUT!!! Files downloaded to a path specified by GetTempFileName may be deleted on
+                // application restart, and as such cannot be relied on for cold updates, only for hot-swaps or
+                // files requiring pre-processing
+                tempLocation = Path.GetTempFileName();
 
-			return fd.DownloadToFile(tempLocation, onProgress);
-		}
+            return fd.DownloadToFile(tempLocation, onProgress);
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
